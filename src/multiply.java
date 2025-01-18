ec51cf0a90f79b6712d32b4f9ba9532a5eec276e
Akash Pandey <akashpande027@gmail.com>
2025-01-18 11:45:04 +0530

    add multiply file

---
+++ src/multiply.java
@@ -0,0 +1,10 @@
+package src;
+
+public class multiply {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = a * b;
+        System.out.println("The product of " + a + " and " + b + " is
+    }
+}
