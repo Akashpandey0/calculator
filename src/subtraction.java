423022148b0e63779bf48a8dc4c1c27bb85b003d
Akash Pandey <akashpande027@gmail.com>
2025-01-18 11:43:36 +0530

    Add subtraction file

---
+++ src/subtraction.java
@@ -0,0 +1,10 @@
+package src;
+
+public class subtraction {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = a - b;
+        System.out.println("The result of the subtraction is: " + c);
+    }
+}
